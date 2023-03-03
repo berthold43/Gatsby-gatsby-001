0625dc8fdad654df46fe849f7244ddbc08fc1478
berthold43 <thommy.johansson@sydmail.se>
2023-03-03 18:21:44 +0100

    Part

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,16 @@
-/**
- * @type {import('gatsby').GatsbyConfig}
- */
 module.exports = {
   siteMetadata: {
-    title: `Gatsby-gatsby-001`,
-    siteUrl: `https://www.yourdomain.tld`,
+    title: "My First Gatsby Site",
   },
-  plugins: [],
-}
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      }
+    },
+  ],
+};
