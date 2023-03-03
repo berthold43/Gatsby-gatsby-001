e2a5d577979055f0549e7ca267c23084e49f51ef
berthold43 <thommy.johansson@sydmail.se>
2023-03-03 12:16:02 +0100

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Gatsby-gatsby-001`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
